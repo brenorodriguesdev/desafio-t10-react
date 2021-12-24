6284eb03e243037cdbd729e7022cee7a73094c84
“Breno <brenorodriguesdev@gmail.com>
2021-12-24 14:31:53 -0300

    feat: implement http client

---
+++ src/infra/axios.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from "../data/contracts/http-client";
+import { api } from "../main/config/axios";
+
+export class Axios implements HttpClient {
+    async post(route: string, body: any): Promise<any> {
+        try {
+            const response = await api.post(route, body)
+            return response.data
+        } catch (error: any) {
+            return new Error(error.response.data.message)
+        }
+    }
+
+    async get(route: string, params: any): Promise<any> {
+        try {
+            const response = await api.get(route, {
+                params
+            })
+            return response.data
+        } catch (error: any) {
+            return new Error(error.response.data.message)
+        }
+    }
+}
