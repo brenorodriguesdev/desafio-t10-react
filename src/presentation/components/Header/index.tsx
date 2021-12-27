c52c5fe9e051fa0b338741f13e16bce476fd5ff8
“Breno <brenorodriguesdev@gmail.com>
2021-12-27 16:54:05 -0300

    feat: add onSend on header

--- src/presentation/components/Header/index.tsx
+++ src/presentation/components/Header/index.tsx
@@ -1,12 +1,35 @@
+import { CreateParticipationUseCase } from '../../../domain/useCases/create-participation';
 import './index.css';
 
-function HeaderComponent() {
+interface HeaderComponentProps {
+  createParticipationUseCase: CreateParticipationUseCase
+}
+
+function HeaderComponent({ createParticipationUseCase }: HeaderComponentProps) {
+
+  async function onSend(event: any) {
+    try {
+      event.preventDefault()
+      const firstName = event.target[0].value
+      const lastName = event.target[1].value
+      const participation = event.target[2].value
+      await createParticipationUseCase.create({
+        firstName,
+        lastName,
+        participation
+      })
+    }
+    catch (error) {
+
+    }
+  }
+
   return (
-    <header className="header">
+    <header className="header" onSubmit={event => onSend(event)}>
       <form>
-        <input className="input" placeholder="First name" />
-        <input className="input" placeholder="Last name" />
-        <input className="input" placeholder="Participation" />
+        <input className="input" placeholder="First name" name="firstName" id="firstName" />
+        <input className="input" placeholder="Last name" name="lastName" id="lastName" />
+        <input className="input" placeholder="Participation" name="participation" id="participation" />
         <input type="submit" className="button" value="SEND" />
       </form>
     </header>
