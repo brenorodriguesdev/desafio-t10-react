1ebb99ae36b0e7df486a905cb2827d13fa667e87
“Breno <brenorodriguesdev@gmail.com>
2021-12-29 06:51:28 -0300

    feat: add grafico on context

--- src/presentation/contexts/participations.tsx
+++ src/presentation/contexts/participations.tsx
@@ -1,20 +1,53 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { ParticipationModel } from "../../domain/models/participation";
 
 interface ParticipationContextProps {
     participations: ParticipationModel[]
     setParticipations: (participations: ParticipationModel[]) => void
+
+    grafico: any
+    setGrafico: (grafico: any) => void
 }
 const ParticipationContext = createContext<ParticipationContextProps>({} as ParticipationContextProps)
 
 function ParticipationProvider({ children }: any) {
     const [participations, setParticipations] = useState<ParticipationModel[]>([])
 
+    const [grafico, setGrafico] = useState({
+        options: {
+            labels: []
+        },
+        series: [],
+    });
+
+    useEffect(() => {
+
+        let labels: any = []
+        let series: any = []
+
+        participations.forEach(participation => labels.push(participation.firstName))
+        participations.forEach(participation => series.push(Number(participation.participation)))
+
+        let options: any = {
+            dataLabels: {
+                enabled: false
+            },
+            labels
+        }
+        
+        setGrafico({
+            options,
+            series,
+        })
+
+    }, [[participations]])
 
     return (
         <ParticipationContext.Provider value={{
             participations,
-            setParticipations
+            setParticipations,
+            grafico,
+            setGrafico
         }}>
             {children}
         </ParticipationContext.Provider>
